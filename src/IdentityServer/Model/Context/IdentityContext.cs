ec07d34e32a32905bf0eae4cda2ac1bdceedb98d
julianodibai <julianodibai2008@gmail.com>
2023-04-08 17:47:21 -0300

    Implementando Identity Server Duende

---
+++ src/IdentityServer/Model/Context/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.IdentityServer.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Model.Context
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext() { }
+
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+
+    }
+}
---
+++ src/IdentityServer/Program.cs
@@ -0,0 +1,76 @@
+using Duende.IdentityServer.AspNetIdentity;
+using Duende.IdentityServer.Services;
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Initializer;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Services;
+using IdentityServer.Model;
+using IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("ProductAPI");
+builder.Services.AddDbContext<IdentityContext>(options => options
+        .UseSqlServer(connectionString),
+        ServiceLifetime.Transient
+        );
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
+var builderServices = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
+
+builderServices.AddDeveloperSigningCredential();
+
+builderServices.AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+var initializer = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializer>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+initializer.Initialize();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
